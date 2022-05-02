205ae89c7e58a4449492cdecff1acf559a9f30de
Nandhini Kandasamy <97506671+Nandhinikandasamy1@users.noreply.github.com>
2022-05-02 17:21:35 +0530

    Update paintcan.py

--- paintcan.py
+++ paintcan.py
@@ -1,9 +1,9 @@
 '''Write program for below scenario using function. You are painting a wall.
 The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall.
  Given a random height and width of wall, calculate how many cans of paint you'll need to buy.'''
-
+import math
 def paint_calc(height,width,cover):
-  number_of_cans = round((height*width)/cover)
+  number_of_cans = math.ceil((height*width)/cover)
   print(f"Number of cans required {number_of_cans}")
 
 test_h = int(input("Height of wall: "))
