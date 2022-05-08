067bb6214fca4d6b02a743d064417a21406b7fcb
Nandhini Kandasamy <97506671+Nandhinikandasamy1@users.noreply.github.com>
2022-05-08 11:24:55 +0530

    Update findduplicateinlist.py

--- findduplicateinlist.py
+++ findduplicateinlist.py
@@ -1,3 +1,6 @@
+'''You are given an array of ids of prisoners. The jail authority found that there are some prisoners of same id. 
+Your task is to help the authority in finding the common ids.'''
+
 N=int(input())
 list1 = input()
 number_list = list1.split()
